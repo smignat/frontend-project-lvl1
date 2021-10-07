2dab529b31e9a8a2c8fa70a6c551a89647f45316
Ignat <ignsm94@gmail.com>
2021-10-07 23:34:21 +0300

    Fixed behaviour  when addition wasn't available

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,7 +3,7 @@ import { getRandomNumber, checkAnswer } from '../index.js';
 
 const getOperation = () => {
   const operations = ['+', '-', '*'];
-  const operationIndex = getRandomNumber(1, 2);
+  const operationIndex = getRandomNumber(0, 2);
   return operations[operationIndex];
 };
 
