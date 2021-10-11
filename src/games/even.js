4d4964fd7cbbc742bb4e27a87fd6ef3a1f695c97
Ignat <ignsm94@gmail.com>
2021-10-11 14:03:26 +0300

    Even function fix

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,6 @@
 import getRandomNumber from '../index.js';
 
-const isEven = (num) => num % 2 === 0;
+const isEven = (num) => !(num % 2);
 
 const evenGame = () => {
   const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
